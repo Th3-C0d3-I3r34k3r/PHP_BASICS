b240dcc1b9b85a13d0de18325a54f0888ceb3cd5
th3-C0d3-I3r34k3r <53413623+Th3-C0d3-I3r34k3r@users.noreply.github.com>
2019-12-08 15:31:49 +0530

    Create 02_variables.php

---
+++ 02_variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ 	<title>My first PHP web page</title>
+ </head>
+
+ <body>
+ 	<?php 
+  $num1=10; // integer
+  $boo_val=True; // boolean value
+  $speed=104.52 //float value
+  $str="the code breaker"  // string
+  echo $num;
+  echo $boo;
+  echo $speed;
+  echo $str;  
+  ?>
+ </body>
+ </html>
